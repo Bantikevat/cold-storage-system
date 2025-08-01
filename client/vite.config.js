378c593c7a634142ccab128a26852e2b32db4a90
Bantikevat <bantikevat199@gmail.com>
2025-08-01 14:08:17 +0530

    all change

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import compression from 'vite-plugin-compression';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    compression(), // Add compression plugin here
+  ],
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase chunk size warning to 1MB
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) return 'react-vendor';      // React chunk
+            if (id.includes('sweetalert2')) return 'swal-vendor';  // Swal
+            if (id.includes('xlsx')) return 'xlsx-vendor';         // Excel export
+            if (id.includes('jspdf') || id.includes('html2canvas')) return 'pdf-vendor'; // PDF tools
+            return 'vendor'; // Default vendor chunk
+          }
+        }
+      }
+    }
+  }
+});
