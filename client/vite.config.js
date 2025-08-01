1bf21bcb96b1ac35c0297df5785d518b1bbd6896
Bantikevat <bantikevat199@gmail.com>
2025-08-01 18:10:07 +0530

    submit

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    include: ['react', 'react-dom']
-  },
-  build: {
-    chunkSizeWarningLimit: 1000
-  },
-  server: {
-    // ✅ Enable React Router fallback for dev
-    historyApiFallback: true
-  },
-  // ✅ Important for static deployment like Render
-  preview: {
-    historyApiFallback: true
-  }
+  plugins: [
+
+    react(),
+    viteStaticCopy({
+      targets: [{ src: 'public/_redirects', dest: '.' }]
+    })
+
+  ]
+  
 });
